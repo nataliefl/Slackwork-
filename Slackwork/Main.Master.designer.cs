240f78795fb3f1b243f24d88ac0d0794a468d73b
magnli10 <lien_magnus@hotmail.com>
2012-02-20 22:43:30 +0100

    Litt layout fiksing paa siden, login system maa ordnes

--- Slackwork/Main.Master.designer.cs
+++ Slackwork/Main.Master.designer.cs
@@ -49,6 +49,33 @@ namespace Slackwork {
         protected global::System.Web.UI.WebControls.LinkButton lbt2;
         
         /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
--- Slackwork/obj/Release/Package/PackageTmp/default.aspx
+++ Slackwork/obj/Release/Package/PackageTmp/default.aspx
@@ -2,7 +2,11 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <h2>Site online at last.</h2>
      <p>
-    Page content, so much page content.
+         <i>20.02.2012</i>
+         <br/> Basic layout of the site, testing asp.net features and such. 
+         Adding gallery with database support is next on the todo, page design isnt decided yet.         
     </p>
+
 </asp:Content>
